9feb8c25c9d8dfa9cc357b862f9be1df662e5885
emreertugrl <emrertugrl@gmail.com>
2025-02-06 14:56:28 +0300

    orders page defined

--- src/app/(pages)/orders/page.tsx
+++ src/app/(pages)/orders/page.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import Order from "@/app/api/models/Order";
+import connectMongo from "@/app/api/utils/connectMongo";
+import { IOrder } from "@/app/api/models/Order";
+import { ICar } from "@/app/api/models/Car";
+import { FC } from "react";
+import OrderCard from "@/app/components/card/order-card";
 
-const orders = () => {
+// Iorder'da product veri tipi ObjectId ama server actions'da product'ı populate ettiğimiz için objectId yerine bütün araç veirlierni aldık buyüzden tipi güncelledik
+export type PopulatedOrder = IOrder & {
+  product: ICar;
+};
+
+const Page: FC = async () => {
+  // server actions ile veritbanına doğrudan client tarafından erişicez
+  const getOrders = async (): Promise<PopulatedOrder[]> => {
+    "use server";
+    await connectMongo();
+    return await Order.find().populate("product");
+  };
+
+  const orders = await getOrders();
+
   return (
-    <div>
-      <h1>Siparişlerim</h1>
+    <div className="px-7 py-7 lg:px-10 text-black">
+      <h1 className="mb-10 text-4xl font-bold">Siparişlerim</h1>
+
+      <div className="grid gap-10">
+        {orders?.map((order) => (
+          <OrderCard key={order._id} order={order} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default orders;
+export default Page;
---
+++ src/app/components/card/order-card.tsx
@@ -0,0 +1,48 @@
+import { PopulatedOrder } from "@/app/(pages)/orders/page";
+import generateImage from "@/app/utils/generateImage";
+import Image from "next/image";
+import { FC } from "react";
+
+type Props = {
+  order: PopulatedOrder;
+};
+
+const OrderCard: FC<Props> = ({ order }) => {
+  return (
+    <div className="border p-5 rounded-md shadow flex gap-5 md:gap-14 items-center hover:bg-zinc-100/10 transition cursor-pointer hover:shadow-lg">
+      <Image
+        src={generateImage(order.product.make, order.product.model)}
+        alt={order.product.model}
+        width={200}
+        height={200}
+        unoptimized
+      />
+
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 flex-1">
+        <h2 className="flex max-xl:flex-col gap-1">
+          <span>{order.product.make}</span>
+          <span className="font-bold xl:ms-2">{order.product.model}</span>
+        </h2>
+
+        <h2 className="flex max-xl:flex-col gap-1">
+          <span>Ödenen Tutar</span>
+          <span className="font-bold xl:ms-2 text-green-600">{order.money_spend}₺</span>
+        </h2>
+
+        <h2 className="flex max-xl:flex-col gap-1">
+          <span>Alım tarihi</span>
+          <span className="font-bold xl:ms-2 text-blue-600">
+            {new Date(order.createdAt).toLocaleDateString("tr")}
+          </span>
+        </h2>
+
+        <h2 className="flex max-xl:flex-col gap-1">
+          <span>Ödeme Tipi</span>
+          <span className="font-bold xl:ms-2 text-blue-600">{order.type}</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default OrderCard;
