e2d29300f1577e7b7e787a864abc3fb934e32b05
emreertugrl <emrertugrl@gmail.com>
2025-02-06 13:06:28 +0300

    loader defined to rent button

--- src/app/components/detail/rent-button.tsx
+++ src/app/components/detail/rent-button.tsx
@@ -2,23 +2,27 @@
 
 import { ICar } from "@/app/api/models/Car";
 import { getPaymentUrl } from "@/app/utils/service";
-import { FC } from "react";
+import { FC, useState } from "react";
+import Loader from "../loader";
 
 type Props = {
   car: ICar;
 };
 
 const RentButton: FC<Props> = ({ car }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const handleRent = async () => {
-    await getPaymentUrl(car).then((url) => (window.location.href = url));
+    await getPaymentUrl(car)
+      .then((url) => (window.location.href = url))
+      .finally(() => setIsLoading(false));
   };
 
   return (
     <button
       onClick={handleRent}
-      className="bg-basic-blue py-2 px-6 text-white rounded-md hover:brightness-90 transition"
+      className="min-w-36 bg-basic-blue py-2 px-6 text-white rounded-md hover:brightness-90 transition"
     >
-      Şimdi Kirala
+      {isLoading ? <Loader /> : "Şimdi Kirala"}
     </button>
   );
 };
